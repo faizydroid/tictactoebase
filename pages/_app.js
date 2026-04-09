24836264e0c24d7cf8c2d6781589c1064374cb58
Your Name <your.email@example.com>
2026-04-10 00:03:22 +0530

    Add Base domain verification meta tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 import { Web3Provider } from '../context/Web3Context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Web3Provider>
-      <Component {...pageProps} />
-    </Web3Provider>
+    <>
+      <Head>
+        <meta name="base:app_id" content="69d67de83966b044539edc43" />
+      </Head>
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
+    </>
   )
 }
 
